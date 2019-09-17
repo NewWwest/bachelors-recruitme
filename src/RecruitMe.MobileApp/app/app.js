22bf535282f06f5f9e5408ec19253e1c695d2e14
Filip Zyskowski <filipzyskowski@gmail.com>
2019-09-17 20:22:17 +0200

    Add navigation router

--- src/RecruitMe.MobileApp/app/app.js
+++ src/RecruitMe.MobileApp/app/app.js
@@ -1,15 +1,30 @@
 import Vue from "nativescript-vue";
 
-import Login from "./components/Login";
+import Home from "./components/Home";
+import Router from "./router";
+
+Vue.prototype.$router = router;
+Vue.prototype.$goto = function (to, options) {
+    var options = options || { 
+        clearHistory: false, 
+        backstackVisible: true, 
+        transition: { 
+            name: "slide",
+            duration: 380,
+            curve: "easeIn"
+        }
+    }
+    this.$navigateTo(this.$router[to], options)
+}
 
 new Vue({
 
     template: `
         <Frame>
-            <Login />
+            <Home />
         </Frame>`,
 
     components: {
-        Login
+        Home
     }
 }).$start();
---
+++ src/RecruitMe.MobileApp/app/router/index.js
@@ -0,0 +1,10 @@
+import Home from './components/Home' 
+import Login from './components/Login' 
+
+const router = { 
+    // add every component to which we will be navigating to
+    home: Home, 
+    login: Login,
+};
+
+export default Router
