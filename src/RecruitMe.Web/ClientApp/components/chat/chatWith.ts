d0542c9f86051e3f2e528ee2851a8537123e4f19
Filip Zyskowski <filipzyskowski@gmail.com>
2020-01-03 20:52:26 +0100

    chat - front improvements

--- src/RecruitMe.Web/ClientApp/components/chat/chatWith.ts
+++ src/RecruitMe.Web/ClientApp/components/chat/chatWith.ts
@@ -3,6 +3,7 @@ import { Component } from 'vue-property-decorator';
 import { UserService } from '../../services/user.service';
 import { MessageService } from '../../services/message.service';
 import { IMessage } from '../../models/chat.models';
+import { isToday, toLocalTime, toLocaleDateTimeString } from '../../helpers/datetime.helper';
 
 @Component({})
 export default class ChatWithComponent extends Vue {
@@ -25,6 +26,12 @@ export default class ChatWithComponent extends Vue {
         this.getMessages();
     }
 
+    mounted() {
+        // always on bottom
+        const el = (this.$refs['mainList'].$el as Element);
+        el.scrollBy(0, el.scrollHeight); 
+    }
+
     sendMessage() {
         this.messageService.sendMessage(this.person, this.msg).then(msg => {
             this.messages.push(msg);
@@ -32,6 +39,45 @@ export default class ChatWithComponent extends Vue {
     }
 
     getMessages() {
+        this.messages = [
+            {
+                isMine: true,
+                message: 'cccc',
+                timestamp: new Date(2020, 0, 1)
+            },
+            {
+                isMine: false,
+                message: 'bbbb',
+                timestamp: new Date(2020, 0, 2)
+            },
+            {
+                isMine: true,
+                message: 'aaaaa',
+                timestamp: new Date(2020, 0, 3)
+            },
+            {
+                isMine: true,
+                message: '00000',
+                timestamp: new Date(2020, 0, 4)
+            },
+            {
+                isMine: true,
+                message: '-1-1-1',
+                timestamp: new Date(2020, 0, 5)
+            },
+            {
+                isMine: false,
+                message: '-2-2-2',
+                timestamp: new Date(2020, 0, 6)
+            },
+            {
+                isMine: true,
+                message: '-3-3-3',
+                timestamp: new Date(2020, 0, 7)
+            },
+            
+        ]
+
         this.messageService.getMessages(this.person, this.page, this.pageSize).then(d => {
             if (d.page * this.pageSize >= d.count) {
                 this.readAll = true;
@@ -46,5 +92,15 @@ export default class ChatWithComponent extends Vue {
         for (let i = 0; i < messagesToAdd.length; i++) {
             this.messages.push(messagesToAdd[i]);
         }
+    }
+
+    getDateText(timestamp: Date): string {
+        const date: Date = toLocalTime(timestamp);
+
+        if (isToday(date)) {
+            return date.toLocaleTimeString();
+        }
+
+        return toLocaleDateTimeString(date);
     }
 }
