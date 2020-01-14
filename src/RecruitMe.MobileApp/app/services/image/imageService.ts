03732eb6b18f2d7760cab01f52b8c868bda99dcc
Filip Zyskowski <filipzyskowski@gmail.com>
2020-01-14 18:03:03 +0100

    choose image from picker and refreshing images

--- src/RecruitMe.MobileApp/app/services/image/imageService.ts
+++ src/RecruitMe.MobileApp/app/services/image/imageService.ts
@@ -2,9 +2,11 @@ import { ApiGateway } from '../common/apiGateway';
 import PopupFactory from '../popupFactory';
 import { LocalStorageService } from '../localStorage/localStorageService';
 import { requestPermissions, takePicture, CameraOptions } from 'nativescript-camera';
+import * as imagepicker from 'nativescript-imagepicker';
 import LoaderService from '../loaderView/loader';
 import { ImageSource, fromUrl } from 'tns-core-modules/image-source/image-source'
 import * as fs from 'tns-core-modules/file-system'
+import { ImageAsset } from 'tns-core-modules/image-asset/image-asset';
 
 export class ImageService {
     private _userImage : ImageSource = new ImageSource();
@@ -39,44 +41,54 @@ export class ImageService {
     public takePicture() : Promise<any> {
         return requestPermissions().then(permissionGranted => {
             return takePicture(ImageService._cameraOptions).then(imageAsset => {
-                LoaderService.showLoader();
-
-                let is: ImageSource = new ImageSource();
-                return is.fromAsset(imageAsset).then(imageSource => {
-                    const folder = fs.knownFolders.documents().path;
-                    const fileName: string = '' + LocalStorageService.getUserId() + '_'
-                        + new Date().getTime() + '_mobile.png';
-                    const path = fs.path.join(folder, fileName);
-
-                    // save file
-                    const saved = imageSource.saveToFile(path, "png");
-                    
-                    if (saved) {
-                        return new Promise( (resolve, reject) => {
-                            console.log("setProfilePicture");
-                            let task = this._apiGateway.setProfilePicture(path, fileName);
-                            console.log("after setProfilePicture");
-
-                            task.on('error', (e) => {
-                                console.log(e);
-                                console.log("error, ale done");
-                                return resolve(false);
-                            });
-                            task.on('complete', (e) => {
-                                console.log(e);
-                                console.log("complete i done");
-                                return resolve(true);
-                            });
-                        })
-                    }
-                    else {
-                        LoaderService.hideLoader();
-                        PopupFactory.GenericErrorPopup('Nie udał się zapis do pamięci telefonu');
-
-                        return new Promise((resolve, reject) => reject(null));
-                    }
-                })
+                return this.saveImageAsset(imageAsset);
             })
+        })
+    }
+
+    public choosePicture() {
+        let context = imagepicker.create({
+            mode: 'single',
+            mediaType: imagepicker.ImagePickerMediaType.Image
+        });
+
+        return context.authorize().then(_ => context.present())
+            .then(selection => {
+                return this.saveImageAsset(selection[0]);
+            })
+    }
+
+    saveImageAsset(imageAsset: ImageAsset): any {
+        LoaderService.showLoader();
+        let is: ImageSource = new ImageSource();
+
+        return is.fromAsset(imageAsset).then(imageSource => {
+            const folder = fs.knownFolders.documents().path;
+            const fileName: string = '' + LocalStorageService.getUserId() + '_'
+                + new Date().getTime() + '_mobile.png';
+            const path = fs.path.join(folder, fileName);
+
+            // save file
+            const saved = imageSource.saveToFile(path, "png");
+            
+            if (saved) {
+                return new Promise( (resolve, reject) => {
+                    let task = this._apiGateway.setProfilePicture(path, fileName);
+
+                    task.on('error', (e) => {
+                        return resolve(false);
+                    });
+                    task.on('complete', (e) => {
+                        return resolve(imageSource);
+                    });
+                })
+            }
+            else {
+                LoaderService.hideLoader();
+                PopupFactory.GenericErrorPopup('Nie udał się zapis do pamięci telefonu');
+
+                return new Promise((resolve, reject) => reject(null));
+            }
         })
     }
 }
