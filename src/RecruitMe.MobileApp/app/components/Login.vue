eef4c1234bd296265e4f22c4519859b592bf60fc
Filip Zyskowski <filipzyskowski@gmail.com>
2020-01-19 19:33:27 +0100

    enable scroll in login page i wrote it this way before, but it seems to be undeterministic xd

--- src/RecruitMe.MobileApp/app/components/Login.vue
+++ src/RecruitMe.MobileApp/app/components/Login.vue
@@ -3,46 +3,48 @@
         <ActionBar class="action-bar">
             <Label class="action-bar-title" text="Login"></Label>
         </ActionBar>
-        <StackLayout class="loginPageBack">
-            <Image class="logoImage"/>
+        <ScrollView>
+            <StackLayout class="loginPageBack">
+                <Image class="logoImage"/>
 
-            <StackLayout class="container">
-                <StackLayout class="inputLoginMargin">
-                    <FlexboxLayout alignItems="center"> 
-                        <Label class="info">
-                            <FormattedString>
-                                <Span class="fa" text.decode="&#xf007; "/>
-                            </FormattedString>
-                        </Label>
-                        <TextField v-model="username" hint="Login" autocorrect="false" autocapitalizationType="none"
-                        :class="[!validUserName() ? 'form-input' : 'error-input', 'input-width']"/>
-                    </FlexboxLayout>
-                    <Label v-show="validUserName()" class="error-label"
-                    text="Login jest wymagany" /> 
+                <StackLayout class="container">
+                    <StackLayout class="inputLoginMargin">
+                        <FlexboxLayout alignItems="center"> 
+                            <Label class="info">
+                                <FormattedString>
+                                    <Span class="fa" text.decode="&#xf007; "/>
+                                </FormattedString>
+                            </Label>
+                            <TextField v-model="username" hint="Login" autocorrect="false" autocapitalizationType="none"
+                            :class="[!validUserName() ? 'form-input' : 'error-input', 'input-width']"/>
+                        </FlexboxLayout>
+                        <Label v-show="validUserName()" class="error-label"
+                        text="Login jest wymagany" /> 
+                    </StackLayout>
+
+                    <StackLayout class="inputPasswordMargin">
+                        <FlexboxLayout alignItems="center">
+                            <Label class="info">
+                                <FormattedString>
+                                    <Span class="fa" text.decode="&#xf023; "/>
+                                </FormattedString>
+                            </Label>
+                            <TextField v-model="password" hint="Password" secure="true" 
+                            v-bind:class="[!validPassword() ? 'form-input' : 'error-input', 'input-width']" />
+                        </FlexboxLayout>
+                        <Label v-show="validPassword()" class="error-label"
+                        text="Hasło jest wymagane" />
+                    </StackLayout>
+
+                    <Button text="Login" @tap="onLoginButtonTap" class="my-button" />
+
+                    <FlexBoxLayout class="form-group" justifyContent="space-between" alignItems="center">
+                        <Label @tap="$goto.RemindLogin()" text="Przypomnij login" />
+                        <Label @tap="$goto.ResetPassword()" text="Zresetuj hasło" />
+                    </FlexBoxLayout>
                 </StackLayout>
-
-                <StackLayout class="inputPasswordMargin">
-                    <FlexboxLayout alignItems="center">
-                        <Label class="info">
-                            <FormattedString>
-                                <Span class="fa" text.decode="&#xf023; "/>
-                            </FormattedString>
-                        </Label>
-                        <TextField v-model="password" hint="Password" secure="true" 
-                        v-bind:class="[!validPassword() ? 'form-input' : 'error-input', 'input-width']" />
-                    </FlexboxLayout>
-                    <Label v-show="validPassword()" class="error-label"
-                    text="Hasło jest wymagane" />
-                </StackLayout>
-
-                <Button text="Login" @tap="onLoginButtonTap" class="my-button" />
-
-                <FlexBoxLayout class="form-group" justifyContent="space-between" alignItems="center">
-                    <Label @tap="$goto.RemindLogin()" text="Przypomnij login" />
-                    <Label @tap="$goto.ResetPassword()" text="Zresetuj hasło" />
-                </FlexBoxLayout>
-            </StackLayout>
-        </StackLayout>       
+            </StackLayout>       
+        </ScrollView>
     </Page>
 </template>
 
